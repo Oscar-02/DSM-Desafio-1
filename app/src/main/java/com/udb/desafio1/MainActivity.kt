dda043df80e93e4c3b61d23a61b824caf47261be
Oscar-02 <oscacanas29gmail.com>
2025-02-22 15:16:51 -0600

    Initial Setup

--- app/src/main/java/com/udb/desafio1/MainActivity.kt
+++ app/src/main/java/com/udb/desafio1/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.udb.desafio1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+lateinit var Plus : Button
+lateinit var Minus : Button
+lateinit var Multiply : Button
+lateinit var Divide : Button
+lateinit var Value1 : EditText
+lateinit var Value2 : EditText
+lateinit var Result : EditText
+
 class MainActivity : AppCompatActivity() {
+    var No1 : Float? = 0f
+    var No2 : Float? = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +27,46 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Plus = findViewById(R.id.plus)
+        Minus = findViewById(R.id.minus)
+        Multiply = findViewById(R.id.multiply)
+        Divide = findViewById(R.id.divide)
+        Value1 = findViewById(R.id.firstValue)
+        Value2 = findViewById(R.id.secondValue)
+        Result = findViewById(R.id.resultValue)
+
+        Plus.setOnClickListener {
+            try {
+                No1 = Value1.text.toString().toFloat()
+                No2 = Value2.text.toString().toFloat()
+            }
+            catch (e:Exception){
+                Result.setText(e.message)
+                return@setOnClickListener
+            }
+
+        }
+    }
+
+
+}
+
+class Operations{
+    fun Sum (val1: Float, val2 : Float) : Float {
+        return val1 + val2;
+    }
+
+    fun Minus (val1: Float, val2 : Float) : Float {
+        return val1 - val2;
+    }
+
+    fun Multiply (val1: Float, val2 : Float) : Float {
+        return val1 * val2;
+    }
+
+    fun Divide (val1: Float, val2 : Float) : Float? {
+        return if (val2 == 0f) null;
+        else val1 / val2;
     }
 }
+
